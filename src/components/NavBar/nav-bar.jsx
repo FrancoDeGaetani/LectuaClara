4b6d829be991da5719a21f1689be535d509f18fb
franco de gaetani <francodegaetani@gmail.com>
2025-06-05 15:41:10 -0300

    haciendo la nav-bat responsiv, se agrego react-icons

--- src/components/NavBar/nav-bar.jsx
+++ src/components/NavBar/nav-bar.jsx
@@ -1,5 +1,7 @@
 
 import { Link } from 'react-router-dom'
+import { FaAnglesDown } from "react-icons/fa6";
+import { useState, useRef  } from 'react';
 
 
 import logo from '../../assets/imgs/logo.png';
@@ -10,13 +12,28 @@ import './nav-bar.css'
 
 function NavBar() {
 
-    const LOGO = logo
+    const [isOpen, setIsOpen] = useState(false);
+    const navBarRef = useRef(null);
+
+    const toggleMenu = () => {
+        setIsOpen(prev => {
+            const newState = !prev;
+            if (navBarRef.current) {
+                navBarRef.current.classList.toggle("nav-bar-open", newState);
+                navBarRef.current.classList.toggle("nav-bar-close", !newState);
+            }
+            return newState;
+        });
+    };
 
     return(
-        <nav className='nav-bar'>
-            <Link to='Biblioteca' className='nav-link'>Biblioteca</Link>
-            <Link to='/'><img src={LOGO} className='nav-bar-logo'></img></Link>
-            <Link to='tu-biblioteca' className='nav-link'>Tu Biblioteca</Link>
+        <nav className='nav-bar' id='nav-bar' ref={navBarRef}>
+            <Link to='/'><img src={logo} className='nav-bar-logo'></img></Link>
+            <div className='nav-links'>
+                <Link to='Biblioteca' className='nav-link'>Biblioteca</Link>
+                <Link to='tu-biblioteca' className='nav-link'>Tu Biblioteca</Link>
+            </div>
+            <button className='nav-button' onClick={toggleMenu}  id='nav-button'><FaAnglesDown /></button>
         </nav>
     )
 }
